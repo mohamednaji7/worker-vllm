677d59edfcaab3e32f06b72316fc8aa8f5660c8e
mohamednaji7 <n4jidx@gmail.com>
2025-01-22 04:47:12 +0200

    chanign the log.info.config

--- src/download_engine_repo.py
+++ src/download_engine_repo.py
@@ -5,7 +5,11 @@ import logging
 
 
 # Setup logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 
 
