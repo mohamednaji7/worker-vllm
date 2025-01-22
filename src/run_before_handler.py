a2a315d21db6084a79d482720bfcce92189ac50f
mohamednaji7 <n4jidx@gmail.com>
2025-01-22 10:33:22 +0200

    cop in tha handelr ro be avaible for import

--- src/run_before_handler.py
+++ src/run_before_handler.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 import logging
-
+import shutil
+import subprocess
 
 # Setup logging
 logging.basicConfig(
@@ -34,7 +35,32 @@ def download_before_handler_scripts_repo():
         logging.error(f"Error during repo download: {str(e)}")
         raise
 
+def copy_scripts_to_src_dir():
+    SCRIPT_REPO_DIR = "./before_handler_script_repo"
+    SCRIPT_SRC_DIR = f"{SCRIPT_REPO_DIR}/src"
+    LOCAL_SCRIPT_DIR = f"{os.getcwd()}/"
+
+    # Ensure the local SCRIPT directory exists before copying files
+    if not os.path.exists(LOCAL_SCRIPT_DIR):
+        os.makedirs(LOCAL_SCRIPT_DIR)
+
+    # Now copy the files
+    for item in os.listdir(SCRIPT_SRC_DIR):
+        src_path = os.path.join(SCRIPT_SRC_DIR, item)
+        dest_path = os.path.join(LOCAL_SCRIPT_DIR, item)
+        
+        if os.path.isdir(src_path):
+            shutil.copytree(src_path, dest_path)
+        else:
+            # Ensure the destination directory exists before copying
+            if not os.path.exists(os.path.dirname(dest_path)):
+                os.makedirs(os.path.dirname(dest_path))
+            shutil.copy2(src_path, dest_path)
+    logging.info("before_handler_script copying completed successfully")
+
+
 def run_before_handler():
     download_before_handler_scripts_repo()
+    copy_scripts_to_src_dir()
     from before_handler_script import before_handler_script
     before_handler_script()
